3effb73d870703dadc39a33f895dcf1c7ba17410
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-11-08 03:56:00 +0000

    chore(release): v3.0.16

---
+++ googlebeta/googlekmscryptokeyversion/googlekmscryptokeyversion_GoogleKmsCryptoKeyVersionTimeouts.go
@@ -0,0 +1,12 @@
+package googlekmscryptokeyversion
+
+
+type GoogleKmsCryptoKeyVersionTimeouts struct {
+	// Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/google-beta/r/google_kms_crypto_key_version#create GoogleKmsCryptoKeyVersion#create}.
+	Create *string `field:"optional" json:"create" yaml:"create"`
+	// Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/google-beta/r/google_kms_crypto_key_version#delete GoogleKmsCryptoKeyVersion#delete}.
+	Delete *string `field:"optional" json:"delete" yaml:"delete"`
+	// Docs at Terraform Registry: {@link https://www.terraform.io/docs/providers/google-beta/r/google_kms_crypto_key_version#update GoogleKmsCryptoKeyVersion#update}.
+	Update *string `field:"optional" json:"update" yaml:"update"`
+}
+
